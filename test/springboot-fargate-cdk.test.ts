cc51870dc1bd7ed7a9153bdd6318be40c80d310b
gino <bfernandez@morris-labs.com>
2024-05-05 09:11:12 -0500

    ecs-cdk

---
+++ test/springboot-fargate-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SpringbootFargateCdk from '../lib/springboot-fargate-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/springboot-fargate-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SpringbootFargateCdk.SpringbootFargateCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
